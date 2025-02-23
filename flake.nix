b4f0f16b1424f6b5cad3bca3187b07a1893ab51e
王虛白 <18016038327@189.cn>
2025-02-23 19:09:28 +0800

    feat: basic binding

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Flake utils demo";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            packages = [ rust-bindgen ];
+            buildInputs = [
+              librime
+            ];
+            nativeBuildInputs = [
+              pkg-config
+              rustup
+              rustPlatform.bindgenHook
+            ];
+
+            RIME_INCLUDE_DIR = "${librime}/include";
+            RIME_LIB_DIR = "${librime}/lib";
+          };
+      }
+    );
+}
